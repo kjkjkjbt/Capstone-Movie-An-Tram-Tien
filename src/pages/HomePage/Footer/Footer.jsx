6966d71313d8207f3d42fb8d013cfae75598afdb
BTGIT <lucywin2610@yahoo.com>
2024-04-12 19:27:10 +0700

    updated cssss

--- src/pages/HomePage/Footer/Footer.jsx
+++ src/pages/HomePage/Footer/Footer.jsx
@@ -6,12 +6,12 @@ export default class Footer extends Component {
         <footer className=" text-white p-4 text-center ">
       <div className="container mx-auto bg-black space-y-3 hover:bg-rose-50">
         <div className="flex justify-center items-center space-x-4 ">
-          <span className="text-white underline decoration-sky-500 hover:text-rose-400	cursor-alias	">Facebook</span>
-          <span className="text-white underline decoration-sky-500 hover:text-rose-400	cursor-alias	">Instagram</span>
-          <span className="text-white underline decoration-sky-500 hover:text-rose-400	cursor-alias	">Pinterest</span>
+          <span className="text-white underline decoration-sky-400 hover:text-rose-400	cursor-alias	">Facebook</span>
+          <span className="text-white underline decoration-sky-400 hover:text-rose-400	cursor-alias	">Instagram</span>
+          <span className="text-white underline decoration-sky-400 hover:text-rose-400	cursor-alias	">Pinterest</span>
         </div>
         <div className="flex justify-center items-center mt-4">
-          <span className="text-white hover:text-rose-400	cursor-zoom-in	"> Information </span>
+          <span className="text-white hover:text-rose-400	cursor-wait	"> Information </span>
         </div>
         <p className="mt-4 text-rose-400 hover:underline cursor-wait">&copy; 2024 Your Company. All rights reserved.</p>
       </div>
