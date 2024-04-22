4b47fc2bd6af9a8d7d1660aa710e58e81074d370
BTGIT <lucywin2610@yahoo.com>
2024-04-22 23:43:12 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screen : { 
+        '2xl:bg-blue-250': {max:'1355px'},
+        'xl:bg-blue-400':{max:'1279px', min :'993px'},
+        'lg:bg-green-250':{max:'992px',min:'768px'},
+        'md:bg-red-500':{max:'767px'},
+        'sm:bg-red-250':{max:'640px'},
+      },
+    },
   },
   plugins: [],
 };
